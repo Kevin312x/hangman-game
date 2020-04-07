d2f1e188f77583afe08d37c5779ef3ad67782a9a
Kevin Huynh <khuynh2@mail.sfsu.edu>
2020-04-07 14:16:51 -0700

    added gui using fltk

--- HangMan/src/Interface.cpp
+++ HangMan/src/Interface.cpp
@@ -1,126 +1,167 @@
-﻿#include <iostream>
-#include <string>
-#include "Interface.h"
+﻿#include "Interface.h"
 
-using std::cout;
-using std::cin;
-using std::endl;
+void HangmanDisplay::draw() {
+    fl_line_style(0, 3);
+    fl_line(x + (WIDTH * 1 / 4), y + (HEIGHT * 2 / 3), x + (WIDTH * 1 / 4) + 200, y + (HEIGHT * 2 / 3));
+    fl_line(x + (WIDTH * 1 / 4) + 150, y + (HEIGHT * 2 / 3), x + (WIDTH * 1 / 4) + 150, y + (HEIGHT * 2 / 3) - 150);
+    fl_line(x + (WIDTH * 1 / 4) + 150, y + (HEIGHT * 2 / 3) - 150, x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 150);
+    fl_line(x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 150, x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 120);
 
-Interface::Interface() : gameOver(false), counter(0) {
-    //These three lines are where the body appears
-    hangman[0] = "|                     |   |";
-    hangman[1] = "|                     |   |";
-    hangman[2] = "|                     |   |";
-}
-
-Interface::~Interface() {}
-
-void Interface::displayInterface() {
-    //To display the interface
-    cout << "+-------------------------+" << endl;
-    cout << "|        Hang Man         |" << endl;
-    cout << "+-------------------------+" << endl;
-    cout << "|            +--------+   |" << endl;
-    cout << "|            |        |   |" << endl;
-    cout << "|            |        |   |" << endl;
-    for (int i = 0; i < 3; i++) {
-        cout << hangman[i] << endl;
+    if (counter >= 1) {
+        // Head
+        fl_circle(x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 115, 5);
     }
-    cout << "|                     |   |" << endl;
-    cout << "|                     |   |" << endl;
-    cout << "|---------------------+---|" << endl;
-    cout << "|                         |" << endl;
-    cout << "+-------------------------+" << endl;
-    cout << "|";
 
-    //Prints out the letters guessed and fills the line with whitespaces
-    for (char c : guessWord) cout << c;
-    for (int i = 0; i < hangman[0].length() - guessWord.length() - 2; i++) cout << " ";
-    cout << "|" << endl;
-}
-
-void Interface::updateInterface() {
-    //Swaps the array with body parts to display on interface
-    switch (counter) {
-    case 1:
-        hangman[0][13] = 'O';
-        break;
-    case 2:
-        hangman[1][13] = '|';
-        break;
-    case 3:
-        hangman[1][12] = '/';
-        break;
-    case 4:
-        hangman[1][14] = '\\';
-        break;
-    case 5:
-        hangman[2][12] = '/';
-        break;
-    case 6:
-        hangman[2][14] = '\\';
-        lose();
-        break;
+    if (counter >= 2) {
+        // Body
+        fl_line(x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 110, x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 90);
     }
-}
 
-void Interface::lose() {
-    //To be printed if player loses
-    cout << "The answer was: " << selectedWord << endl;
-    cout << "You Lose!" << endl;
-    gameOver = true;
-}
+    if (counter >= 3) {
+        // Left arm
+        fl_line(x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 110, x + (WIDTH * 1 / 4) + 90, y + (HEIGHT * 2 / 3) - 100);
+    }
 
-void Interface::win() {
-    //To be printed if player wins
-    cout << "You Win!" << endl;
-    gameOver = true;
-}
+    if (counter >= 4) {
+        // Right arm
+        fl_line(x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 110, x + (WIDTH * 1 / 4) + 110, y + (HEIGHT * 2 / 3) - 100);
+    }
 
-/*
-Checks if user input, if it is a character, matches any
-of the characters of the selected word.
-If it isn't a character, the it matches the selected word itself.
-*/
-void Interface::check(const std::string& input) {
-    bool containsLetter = false;
-    bool equal = true;
-    if (input.size() == 0) {
-        return;
-    } else if (input.size() < 2) {
-        if (std::find(guessed_letters.begin(), guessed_letters.end(), input[0]) != guessed_letters.end()) {
-            return;
+    if (counter >= 5) {
+        // Left leg
+        fl_line(x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 90, x + (WIDTH * 1 / 4) + 90, y + (HEIGHT * 2 / 3) - 80);
+    }
+
+    if (counter >= 6) {
+        // Right leg
+        fl_line(x + (WIDTH * 1 / 4) + 100, y + (HEIGHT * 2 / 3) - 90, x + (WIDTH * 1 / 4) + 110, y + (HEIGHT * 2 / 3) - 80);
+    }
+
+}
+void readFile(std::string& selectedWord, std::string& guessWord) {
+    std::ifstream file;
+    std::string line;
+    int random = 0;
+    int lineNum = 0;
+    file.open("wordlist.txt");
+
+    srand(time(0));
+    random = (rand() % 212) + 1;
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            lineNum++;
+            if (lineNum == random) {
+                selectedWord = line;
+                for (int i = 0; i < selectedWord.length(); i++) {
+                    guessWord += '_';
+                }
+                std::cout << selectedWord << '\n' << guessWord << '\n';
+                file.close();
+            }
         }
-        guessed_letters.push_back(input[0]);
-        for (int i = 0; i < selectedWord.length(); i++) {
-            
+    }
+}
+
+Interface::Interface() {
+    readFile(selectedWord, guessWord);
+    display = new HangmanDisplay(0, 0, WIDTH, HEIGHT);
+    window = new Fl_Window(WIDTH, HEIGHT, "HangMan");
+    box = new Fl_Box(0, 0, WIDTH, 35, guessWord.c_str());
+    box->box(FL_UP_BOX);
+    input = new Fl_Input(100, 250, 200, 30);
+    button = new Fl_Button(310, 250, 50, 30, "Enter");
+    button->callback(static_button_CB, this);
+    window->add(display);
+}
+
+void Interface::run() {
+    window->show();
+    Fl::run();
+}
+
+void Interface::reset() {
+    display->counter = 0;
+    readFile(selectedWord, guessWord);
+    display->redraw();
+}
+
+// Returns -1 if there is no input or input was guessed previously
+// Returns 0 if selected word does not contain input
+// Returns 1 if selected word contains input
+// Returns 2 if selected word is input
+int Interface::check(const std::string& input) {
+    int ret_val = 0;
+
+    if (input.size() == 0)
+        ret_val = -1;
+    else if (input.size() > 1) {
+        if (selectedWord == input) {
+            guessWord = selectedWord;
+            ret_val = 2;
+        }
+    }
+    else {
+        for (int i = 0; i < selectedWord.length(); ++i) {
             if (selectedWord[i] == input[0]) {
                 guessWord[i] = input[0];
-                containsLetter = true;
+                ret_val = 1;
             }
         }
-        if (containsLetter) {
-            if (guessWord.find('_') == std::string::npos) {
-                win();
-            }
-        } else {
-            incrementCounter();
-            updateInterface();
-        }
-    } else {
-        if (input == selectedWord) {
-            win();
-        } else {
-            incrementCounter();
-            updateInterface();
-        }
+    }
+
+    return ret_val;
+}
+
+void Interface::static_button_CB(Fl_Widget* w, void* data) {
+    ((Interface*)data)->real_button_CB(w);
+}
+
+void Interface::real_button_CB(Fl_Widget* w) {
+    std::string guess = input->value();
+    int val = check(guess);
+    input->value("");
+
+    if (val == -1) {
+        return;
+    }
+    if (val == 0) {
+        display->counter += 1;
+        display->redraw();
+    }
+    if (val >= 1) {
+        box->label(guessWord.c_str());
+    }
+
+    if (selectedWord == guessWord) {
+        //win
+        GameOver win(200, 200, "win", this);
+    }
+    else if (display->counter >= 6) {
+        //lose
+        GameOver lose(200, 200, "lose", this);
     }
 }
 
-int Interface::getCounter() {
-    return counter;
+GameOver::GameOver(int w, int h, const char* t, Interface* inter) : Fl_Window(w, h, t) {
+    i = inter;
+    wind = new Fl_Window(w, h, t);
+    ok = new Fl_Button(40, 150, 50, 25, "OK");
+    ok->callback(okbutton_CB);
+    //reset = new Fl_Button(100, 150, 50, 25, "Reset");
+    //reset->callback(static_resetbutton_CB, this);
+    wind->show();
 }
 
-void Interface::incrementCounter() {
-    counter += 1;
+void GameOver::okbutton_CB(Fl_Widget* w, void* data) {
+    exit(0);
+}
+
+void GameOver::static_resetbutton_CB(Fl_Widget* w, void* data) {
+    ((GameOver*)data)->real_resetbutton_CB(w);
+}
+
+void GameOver::real_resetbutton_CB(Fl_Widget* w) {
+    i->reset();
+    wind->end();
 }
--- HangMan/src/Interface.h
+++ HangMan/src/Interface.h
@@ -1,28 +1,61 @@
+#pragma once
+#include <windows.h>
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+#include <FL/Fl_Input.H>
+#include <FL/Fl_Widget.H>
+#include <FL/fl_draw.H>
 #include <iostream>
-#include <string>
+#include <ctime>
 #include <vector>
+#include <fstream>
+#include <string>
 
-#ifndef INTERFACE_H
-#define INTERFACE_H
+constexpr int WIDTH = 400;
+constexpr int HEIGHT = 300;
 
-class Interface {
-public:
-    Interface();
-    bool gameOver;
-    std::string selectedWord;
-    std::string guessWord;
-    std::string* hangman = new std::string[3];
-    ~Interface();
-    void displayInterface();
-    void updateInterface();
-    void lose();
-    void win();
-    void check(const std::string& input);
-    int getCounter();
-    void incrementCounter();
+//void readFile(std::string& selectedWord, std::string& guessWord);
+
+struct HangmanDisplay : public Fl_Widget {
+    HangmanDisplay(int x, int y, int w, int h) : Fl_Widget(x, y, w, h), x(x), y(y), w(w), h(h) {}
+    void draw();
+
+    int counter = 0;
 private:
-    int counter;
-    std::vector<char> guessed_letters;
+    int x, y, w, h;
+
 };
 
-#endif
+struct Interface {
+    Interface();
+    void run();
+    void reset();
+private:
+    std::string selectedWord, guessWord;
+    HangmanDisplay* display;
+    Fl_Window* window;
+    Fl_Box* box;
+    Fl_Input* input;
+    Fl_Button* button;
+
+    static void static_button_CB(Fl_Widget* w, void* data);
+    void real_button_CB(Fl_Widget* w);
+    int check(const std::string& input);
+    //void win();
+    //void lose();
+};
+
+struct GameOver : public Fl_Window {
+    GameOver(int w, int h, const char* t, Interface* inter);
+private:
+    Fl_Window* wind;
+    Fl_Button* ok;
+    Fl_Button* reset;
+    Interface* i;
+
+    static void okbutton_CB(Fl_Widget* w, void* data);
+    static void static_resetbutton_CB(Fl_Widget* w, void* data);
+    void real_resetbutton_CB(Fl_Widget* w);
+};
